789066adda5d720249e761c5a34db916e762f347
Oldarorn <antoine.niveau2@gmail.com>
2018-02-27 12:27:36 +0100

    bye bye useless files

--- __resource.lua
+++ __resource.lua
@@ -1,10 +1,8 @@
-version '2.0.0'
+version '1.0.0'
 
 server_scripts {
 	'@mysql-async/lib/MySQL.lua',
 	'@es_extended/locale.lua',
-	'locales/de.lua',
-	'locales/en.lua',
 	'locales/fr.lua',
 	'config.lua',
 	'server/main.lua'
@@ -12,8 +10,6 @@ server_scripts {
 
 client_scripts {
 	'@es_extended/locale.lua',
-	'locales/de.lua',
-	'locales/en.lua',
 	'locales/fr.lua',
 	'config.lua',
 	'client/main.lua'
