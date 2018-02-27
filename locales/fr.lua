2ed63db8666466e817ac0fc04ecfb0bbcb5ea9ba
Oldarorn <antoine.niveau2@gmail.com>
2018-02-27 02:13:31 +0100

    first commit

---
+++ locales/fr.lua
@@ -0,0 +1,15 @@
+Locales['fr'] = {
+
+	['by_default'] = 'par défaut',
+	['installed'] = 'installé',
+	['already_own'] = 'vous possédez déjà: ~b~',
+	['not_enough_money'] = 'vous n\'avez pas assez d\'argent sale!',
+	['purchased'] = 'achat effectué !',
+	['press_custom'] = 'appuyez sur ~INPUT_PICKUP~ pour personnaliser le véhicule.',
+	['level'] = 'niveau ',
+-- Upgrades
+	['upgrades'] = 'upgrades',
+	['armor'] = 'armure',
+	['turbo'] = 'turbo',
+	['no_turbo'] = 'pas de turbo',
+}
